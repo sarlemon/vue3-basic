dd25d71c296b675e7192175613d3942d76fa0817
sarlemon <406401437@qq.com>
2020-12-13 22:06:14 +0800

    vue3学习

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="container">
+    <column-list :list="list"></column-list>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import ColumnList, { ColumnProps } from './components/ColumnList.vue'
+const testData: ColumnProps[] = [
+  {
+    id: 1,
+    title: 'test1的专栏',
+    description: '这是的test1专栏，有一段非常有意思的简介，可以更新一下欧',
+    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5f3e41adb7d9c60b68cdd1ed.jpg'
+  },
+  {
+    id: 2,
+    title: 'test2的专栏',
+    description: '这是的test2专栏，有一段非常有意思的简介，可以更新一下欧',
+    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5f3e41adb7d9c60b68cdd1ed.jpg'
+  }
+]
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    ColumnList
+  },
+  setup() {
+    return {
+      list: testData
+    }
+  }
+});
+</script>
+
+<style>
+
+</style>
