4c8bdcb648adbc3ee939c78b347d36f4bdefa529
akio <406401437@qq.com>
2020-12-14 23:59:49 +0800

    下拉菜单组件开发

--- src/components/GlobalHeader.vue
+++ src/components/GlobalHeader.vue
@@ -11,7 +11,11 @@
         </ul>
         <ul v-else class="list-inline mb-0">
             <li class="list-inline-item">
-                <a class="btn btn-outline-light my-2">你好 {{user.name}}</a>
+                <dropdown :title="`你好 ${user.name}`">
+                    <dropdown-item><a href="#" class="dropdown-item">新建文章</a></dropdown-item>
+                    <dropdown-item disabled><a href="#" class="dropdown-item">编辑资料</a></dropdown-item>
+                    <dropdown-item><a href="#" class="dropdown-item">退出登陆</a></dropdown-item>
+                </dropdown>
             </li>
         </ul>
     </nav>
@@ -19,6 +23,8 @@
 
 <script lang="ts">
 import {defineComponent, PropType} from 'vue'
+import Dropdown from './Dropdown.vue'
+import DropdownItem from './DropdownItem.vue'
 export interface UserProps {
     isLogin: boolean;
     name?: string;
@@ -26,6 +32,10 @@ export interface UserProps {
 }
 export default defineComponent({
     name: 'GlobalHeader',
+    components: {
+        Dropdown,
+        DropdownItem
+    },
     props: {
         user: {
             type: Object as PropType<UserProps>,
