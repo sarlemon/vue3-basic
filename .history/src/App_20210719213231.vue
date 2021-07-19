75c74b4058fcd867a52baa49dfd757accc69f34f
akio <406401437@qq.com>
2021-07-19 22:50:47 +0800

    refactor: 💡 代码重构

--- .history/src/App_20210719213231.vue
+++ .history/src/App_20210719213231.vue
@@ -1,22 +1,18 @@
 <template>
   <div class="container">
     <global-header :user="currentUser"></global-header>
-    <form action>
+    <form action="">
       <div class="mb-3">
         <label for="exampleInputEmail1" class="form-label">邮箱地址</label>
-        <input
-          type="email"
-          class="form-control"
-          id="exampleInputEmail1"
-          aria-describedby="emailHelp"
+        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
           v-model="emailRef.val"
           @blur="validateEmail"
-        />
+        >
         <div class="form-text" v-if="emailRef.error">{{emailRef.message}}</div>
       </div>
       <div class="mb-3">
         <label for="exampleInputPassword1" class="form-label">密码</label>
-        <input type="password" class="form-control" id="exampleInputPassword1" />
+        <input type="password" class="form-control" id="exampleInputPassword1">
       </div>
     </form>
   </div>
@@ -24,13 +20,13 @@
 
 <script lang="ts">
 import { defineComponent, reactive } from 'vue';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import ColumnList, { ColumnProps } from './components/ColumnList.vue';
-import GlobalHeader, { UserProps } from './components/GlobalHeader.vue';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import ColumnList, { ColumnProps } from './components/ColumnList.vue'
+import GlobalHeader, { UserProps } from './components/GlobalHeader.vue'
 const currentUser: UserProps = {
   isLogin: true,
-  name: 'Akio'
-};
+  name:'Akio',
+}
 const testData: ColumnProps[] = [
   {
     id: 1,
@@ -61,9 +57,8 @@ const testData: ColumnProps[] = [
     description: '这是的test5专栏，有一段非常有意思的简介，可以更新一下欧',
     avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5f3e41adb7d9c60b68cdd1ed.jpg'
   }
-];
-const emailReg =
-  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+]
+const emailReg = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 export default defineComponent({
   name: 'App',
   components: {
@@ -75,25 +70,26 @@ export default defineComponent({
       val: '',
       error: false,
       message: ''
-    });
+    })
     const validateEmail = () => {
-      if (emailRef.val.trim() === '') {
-        emailRef.error = true;
-        emailRef.message = 'can not be empty';
-      } else if (!emailReg.test(emailRef.val)) {
-        emailRef.error = true;
-        emailRef.message = 'should be valid message';
+      if(emailRef.val.trim() === '') {
+        emailRef.error = true
+        emailRef.message = 'can not be empty'
+      } else if(!emailReg.test(emailRef.val)) {
+        emailRef.error = true
+        emailRef.message = 'should be valid message'
       }
-    };
+    }
     return {
       list: testData,
       currentUser,
       emailRef,
       validateEmail
-    };
+    }
   }
 });
 </script>
 
 <style>
+
 </style>
