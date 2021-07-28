e23e6a3817b1366bfbed9135d38ae8f07ac554c0
akio <406401437@qq.com>
2021-07-29 00:32:22 +0800

    feat: 🎸 表单校验逻辑封装

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,10 @@
     <global-header :user="currentUser"></global-header>
     <form action>
       <div class="mb-3">
+        <label class="form-label">邮箱地址</label>
+        <validate-input :rules="emailRules"></validate-input>
+      </div>
+      <div class="mb-3">
         <label for="exampleInputEmail1" class="form-label">邮箱地址</label>
         <input
           type="email"
@@ -26,6 +30,7 @@
 import { defineComponent, reactive } from 'vue';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ColumnList, { ColumnProps } from './components/ColumnList.vue';
+import ValidateInput, { RulesProp } from './components/ValidateInput.vue';
 import GlobalHeader, { UserProps } from './components/GlobalHeader.vue';
 const currentUser: UserProps = {
   isLogin: true,
@@ -68,9 +73,14 @@ export default defineComponent({
   name: 'App',
   components: {
     // ColumnList,
-    GlobalHeader
+    GlobalHeader,
+    ValidateInput
   },
   setup() {
+    const emailRules: RulesProp = [
+      { type: 'required', message: '电子邮箱地址不能为空' },
+      { type: 'email', message: '请输入正确的电子邮箱格式' }
+    ];
     const emailRef = reactive({
       val: '',
       error: false,
@@ -89,7 +99,8 @@ export default defineComponent({
       list: testData,
       currentUser,
       emailRef,
-      validateEmail
+      validateEmail,
+      emailRules
     };
   }
 });
